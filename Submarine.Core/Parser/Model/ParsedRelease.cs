92b50e27af775a63d8556114b12198aa809e540b
Yukine <devyukine@gmx.de>
2021-07-29 01:18:13 +0200

    feat(Parser): add parser models

---
+++ Submarine.Core/Parser/Model/ParsedRelease.cs
@@ -0,0 +1,15 @@
+﻿using Submarine.Core.Quality;
+
+namespace Submarine.Core.Parser.Model
+{
+	public record ParsedRelease
+	{
+		public string ReleaseTitle { get; set; }
+		
+		public string SeriesTitle { get; set; }
+		
+		public ReleaseType Type { get; set; }
+		
+		public QualityModel Quality { get; set; }
+	};
+}
---
+++ Submarine.Core/Parser/Model/ReleaseType.cs
@@ -0,0 +1,18 @@
+﻿namespace Submarine.Core.Parser.Model
+{
+	public enum ReleaseType
+	{
+		SERIES,
+		MOVIE,
+		UNKNOWN
+	}
+
+	public enum SeriesReleaseType
+	{
+		EPISODE,
+		SPECIAL,
+		FULL_SEASON,
+		PARTIAL_SEASON,
+		MULTI_SEASON,
+	}
+}
