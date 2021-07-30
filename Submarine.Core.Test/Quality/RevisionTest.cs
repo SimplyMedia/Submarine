6eee84926a17a0a459733105a17fa03ad9b85e5d
Yukine <devyukine@gmx.de>
2021-07-30 04:00:23 +0200

    test(Revision): add unit tests

---
+++ Submarine.Core.Test/Quality/RevisionTest.cs
@@ -0,0 +1,21 @@
+using Submarine.Core.Quality;
+using Xunit;
+
+namespace Submarine.Core.Test.Quality
+{
+	public class RevisionTest
+	{
+		[Theory]
+		[InlineData(1, 2, 1)]
+		[InlineData(2, 1, -1)]
+		[InlineData(1, 1, 0)]
+		[InlineData(null, 1, 1)]
+		public void Check_CompareRevisionToInitial_AsExpected(int? first, int second, int result)
+		{
+			var firstRevision = first != null ? new Revision((int) first) : null;
+			var secondRevision = new Revision(second);
+			
+			Assert.Equal(result, secondRevision.CompareTo(firstRevision));
+		}
+	}
+}
