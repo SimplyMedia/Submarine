543e21a8d71717dcc9cdb80102d43ba7fd42d071
Yukine <devyukine@gmx.de>
2021-08-24 00:27:47 +0200

    test(QualityResolution): improve naming

--- Submarine.Core.Test/Quality/QualityResolutionTest.cs
+++ Submarine.Core.Test/Quality/QualityResolutionTest.cs
@@ -6,7 +6,7 @@ namespace Submarine.Core.Test.Quality
 	public class QualityResolutionTest
 	{
 		[Fact]
-		public void All_ShouldNotBeEmpty() 
+		public void All_ShouldReturnMoreThanOneEntry_WhenAccessed() 
 			=> Assert.True(QualityResolutionModel.All.Length > 0);
 	}
 }
