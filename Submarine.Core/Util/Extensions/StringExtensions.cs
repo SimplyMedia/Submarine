143a969a2bbfca0b7c86621ce2acefa0c68cb894
Yukine <devyukine@gmx.de>
2022-01-09 06:38:00 +0100

    style(StringExtension): reformat code

--- Submarine.Core/Util/Extensions/StringExtensions.cs
+++ Submarine.Core/Util/Extensions/StringExtensions.cs
@@ -1,14 +1,17 @@
-namespace Submarine.Core.Util.Extensions
+namespace Submarine.Core.Util.Extensions;
+
+public static class StringExtensions
 {
-	public static class StringExtensions
+	public static string Reverse(this string str)
 	{
-		public static string Reverse(this string str) {  
-			var chars = new char[str.Length];  
-			for (int i = 0, j = str.Length - 1; i <= j; i++, j--) {  
-				chars[i] = str[j];  
-				chars[j] = str[i];  
-			}  
-			return new string(chars);  
-		}  
+		var chars = new char[str.Length];
+		for (int i = 0, j = str.Length - 1; i <= j; i++, j--)
+		{
+			chars[i] = str[j];
+			chars[j] = str[i];
+		}
+
+		return new string(chars);
+	}
 	}
 }
