848962e1ef38dec1d0cb056331ed475411b31d69
Yukine <devyukine@gmx.de>
2022-01-13 02:19:36 +0100

    feat(Revision): add compare operators & Equals

--- Submarine.Core.Test/Quality/RevisionTest.cs
+++ Submarine.Core.Test/Quality/RevisionTest.cs
@@ -10,25 +10,89 @@ public class RevisionTest
 	[InlineData(2, 4, 1)]
 	[InlineData(1, 3, 1)]
 	public void CompareTo_ShouldReturnPositive_WhenSecondRevisionIsHigher(int? first, int second, int result)
-		=> AssertRevision(first, second, result);
+		=> AssertCompareTo(first, second, result);
 
 	[Theory]
 	[InlineData(2, 1, -1)]
 	[InlineData(4, 2, -1)]
 	[InlineData(3, 1, -1)]
 	public void CompareTo_ShouldReturnNegative_WhenFirstRevisionIsHigher(int? first, int second, int result)
-		=> AssertRevision(first, second, result);
+		=> AssertCompareTo(first, second, result);
 
 	[Theory]
 	[InlineData(1, 1, 0)]
+	[InlineData(2, 2, 0)]
+	[InlineData(3, 3, 0)]
 	public void CompareTo_ShouldReturnZero_WhenBothRevisionsAreTheSame(int? first, int second, int result)
-		=> AssertRevision(first, second, result);
+		=> AssertCompareTo(first, second, result);
 
 	[Fact]
 	public void CompareTo_ShouldReturnPositive_WhenFirstRevisionIsNull()
-		=> AssertRevision(null, 1, 1);
+		=> AssertCompareTo(null, 1, 1);
 
-	private void AssertRevision(int? first, int second, int result)
+	[Theory]
+	[InlineData(2, 1)]
+	[InlineData(4, 2)]
+	[InlineData(3, 1)]
+	public void GreaterOperator_ShouldReturnTrue_WhenLeftIsGreater(int first, int second)
+		=> Assert.True(new Revision(first) > new Revision(second));
+
+	[Fact]
+	public void GreaterOperator_ShouldReturnFalse_WhenLeftIsNull()
+		=> Assert.False(null > new Revision());
+
+	[Fact]
+	public void GreaterOperator_ShouldReturnTrue_WhenRightIsNull()
+		=> Assert.True(new Revision() > null);
+
+	[Theory]
+	[InlineData(1, 3)]
+	[InlineData(0, 1)]
+	[InlineData(2, 4)]
+	public void LesserOperator_ShouldReturnTrue_WhenRightIsGreater(int first, int second)
+		=> Assert.True(new Revision(first) < new Revision(second));
+
+	[Fact]
+	public void LesserOperator_ShouldReturnTrue_WhenLeftIsNull()
+		=> Assert.True(null < new Revision());
+
+	[Fact]
+	public void LesserOperator_ShouldReturnFalse_WhenRightIsNull()
+		=> Assert.False(new Revision() < null);
+
+	[Theory]
+	[InlineData(2, 1)]
+	[InlineData(4, 2)]
+	[InlineData(3, 1)]
+	[InlineData(2, 2)]
+	public void GreaterOrEqualOperator_ShouldReturnTrue_WhenLeftIsGreaterOrEqual(int first, int second)
+		=> Assert.True(new Revision(first) >= new Revision(second));
+
+	[Fact]
+	public void GreaterOrEqualOperator_ShouldReturnFalse_WhenLeftIsNull()
+		=> Assert.False(null >= new Revision());
+
+	[Fact]
+	public void GreaterOrEqualOperator_ShouldReturnTrue_WhenRightIsNull()
+		=> Assert.True(new Revision() >= null);
+
+	[Theory]
+	[InlineData(1, 3)]
+	[InlineData(0, 1)]
+	[InlineData(2, 4)]
+	[InlineData(2, 2)]
+	public void LesserOrEqualOperator_ShouldReturnTrue_WhenRightIsGreaterOrEqual(int first, int second)
+		=> Assert.True(new Revision(first) <= new Revision(second));
+
+	[Fact]
+	public void LesserOrEqualOperator_ShouldReturnTrue_WhenLeftIsNull()
+		=> Assert.True(null <= new Revision());
+
+	[Fact]
+	public void LesserOrEqualOperator_ShouldReturnFalse_WhenRightIsNull()
+		=> Assert.False(new Revision() <= null);
+
+	private static void AssertCompareTo(int? first, int second, int result)
 	{
 		var firstRevision = first != null ? new Revision((int)first) : null;
 		var secondRevision = new Revision(second);
--- Submarine.Core/Quality/Revision.cs
+++ Submarine.Core/Quality/Revision.cs
@@ -22,4 +22,49 @@ public record Revision(
 
 		return 0;
 	}
+
+	public virtual bool Equals(Revision? other)
+	{
+		if (ReferenceEquals(null, other)) return false;
+		if (ReferenceEquals(this, other)) return true;
+		return Version == other.Version
+		       && IsRepack == other.IsRepack
+		       && IsProper == other.IsProper
+		       && IsReal == other.IsReal;
+	}
+
+	public override int GetHashCode()
+		=> HashCode.Combine(Version, IsRepack, IsProper, IsReal);
+
+	public static bool operator >(Revision? left, Revision? right)
+	{
+		if (ReferenceEquals(null, left)) return false;
+		if (ReferenceEquals(null, right)) return true;
+
+		return left.CompareTo(right) > 0;
+	}
+
+	public static bool operator <(Revision? left, Revision? right)
+	{
+		if (ReferenceEquals(null, left)) return true;
+		if (ReferenceEquals(null, right)) return false;
+
+		return left.CompareTo(right) < 0;
+	}
+
+	public static bool operator >=(Revision? left, Revision? right)
+	{
+		if (ReferenceEquals(null, left)) return false;
+		if (ReferenceEquals(null, right)) return true;
+
+		return left.CompareTo(right) >= 0;
+	}
+
+	public static bool operator <=(Revision? left, Revision? right)
+	{
+		if (ReferenceEquals(null, left)) return true;
+		if (ReferenceEquals(null, right)) return false;
+
+		return left.CompareTo(right) <= 0;
+	}
 }
