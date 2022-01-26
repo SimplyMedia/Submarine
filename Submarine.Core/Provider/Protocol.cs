b4de6eac5c74ccd3b5faedc4c7d1efa80a092de8
Yukine <devyukine@gmx.de>
2022-01-26 03:59:47 +0100

    docs(Protocol): add xml docs

--- Submarine.Core/Provider/Protocol.cs
+++ Submarine.Core/Provider/Protocol.cs
@@ -1,8 +1,22 @@
 namespace Submarine.Core.Provider;
 
+/// <summary>
+///     Supported Protocols of a <see cref="Provider" />
+/// </summary>
 public enum Protocol
 {
+	/// <summary>
+	///     The <see href="https://en.wikipedia.org/wiki/BitTorrent">Bittorrent</see> Protocol
+	/// </summary>
 	BITTORRENT,
+
+	/// <summary>
+	///     The <see href="https://en.wikipedia.org/wiki/Usenet">Usenet</see> Protocol
+	/// </summary>
 	USENET,
+
+	/// <summary>
+	///     The <see href="https://en.wikipedia.org/wiki/XDCC">XDCC</see> Protocol, based on IRC
+	/// </summary>
 	XDCC
 }
