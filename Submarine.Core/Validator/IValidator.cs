0eb80511cdefad8b3c7f084661dd246abd6a91e7
Yukine <devyukine@gmx.de>
2022-01-26 04:00:17 +0100

    docs(IValidator): add xml docs

--- Submarine.Core/Validator/IValidator.cs
+++ Submarine.Core/Validator/IValidator.cs
@@ -1,6 +1,14 @@
 namespace Submarine.Core.Validator;
 
+/// <summary>
+///     IValidator validates the given input is correct and valid
+/// </summary>
+/// <typeparam name="T">Type of input</typeparam>
 public interface IValidator<in T>
 {
+	/// <summary>
+	///     Validate that <see cref="input" /> is valid
+	/// </summary>
+	/// <param name="input">the input to validate</param>
 	void Validate(T input);
 }
