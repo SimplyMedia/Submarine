0aa9d8ff00acd2393f1009184938b02eb6c8b3c1
Yukine <devyukine@gmx.de>
2022-02-23 04:14:14 +0100

    docs(SeriesReleaseType): add xml docs

--- Submarine.Core/Release/SeriesReleaseType.cs
+++ Submarine.Core/Release/SeriesReleaseType.cs
@@ -1,10 +1,32 @@
 namespace Submarine.Core.Release;
 
+/// <summary>
+/// The Type of a Series Release
+/// </summary>
 public enum SeriesReleaseType
 {
+	/// <summary>
+	/// Release is an episode of a Series
+	/// </summary>
 	EPISODE,
+	
+	/// <summary>
+	/// Release is a special episode of a Series 
+	/// </summary>
 	SPECIAL,
+	
+	/// <summary>
+	/// Release is a full season of a Series
+	/// </summary>
 	FULL_SEASON,
+	
+	/// <summary>
+	/// Release is a partial season of this Series
+	/// </summary>
 	PARTIAL_SEASON,
+	
+	/// <summary>
+	/// Release is containing multiple seasons of this Series
+	/// </summary>
 	MULTI_SEASON
 }
